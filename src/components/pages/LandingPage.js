ed7dfacea360355360bbb86892ebc23d734e091f
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-01 17:04:46 +1000

    Updated fonts, added hero text to landing page as well as created basic animations

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -3,6 +3,17 @@ import styled from "@emotion/styled";
 import backgroundImage from "../../assets/Web_Final_Col.jpg";
 import NavComponent from "../Nav";
 import FooterComponent from "../Footer";
+import { keyframes } from "@emotion/core";
+
+const fadeInAnimation = keyframes`
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}`;
 
 const LandingPageContainer = styled.main`
   display: flex;
@@ -13,11 +24,35 @@ const LandingPageContainer = styled.main`
   width: 100vw;
 `;
 
+const HeroTextContainer = styled.div`
+  animation: ${fadeInAnimation} ease 3s;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  background-color: transparent;
+  color: #493a37;
+  margin-left: 18%;
+`;
+
+const Heading = styled.h1`
+  margin-top: 30%;
+  font-size: 6em;
+`;
+
+const SubHeading = styled.h2`
+  font-size: 3em;
+`;
+
 function LandingPage() {
   return (
     <LandingPageContainer>
       <NavComponent />
-      <p>home page</p>
+      <HeroTextContainer>
+        <Heading>HELLO.</Heading>
+        <SubHeading>my name's Chris.</SubHeading>
+      </HeroTextContainer>
       <FooterComponent />
     </LandingPageContainer>
   );
