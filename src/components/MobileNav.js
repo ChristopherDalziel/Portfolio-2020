f7fefe86a78e9b2d171632396ac8f1548ad9dfa6
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-04 00:29:01 +1000

    Added hover functionality to burger nav, added logo, updated css

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styled from "@emotion/styled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const BurgerMenu = styled.nav`
   background: pink;
+  width: 100px;
   padding: 5px;
   position: absolute;
   z-index: 1;
@@ -13,39 +16,54 @@ const BurgerMenu = styled.nav`
     display: none;
 `;
 
+const BurgerIconHover = styled.div`
+  margin: 0 0 0 5px;
+  position: absolute;
+  font-size: 30px;
+`;
+
 const BurgerText = styled.p`
-  list-style-type: none;
-  text-decoration: none;
+  font-size: 12px;
   color: #232323;
   transition: color 0.3s ease;
 `;
 
 const MobileNav = () => {
+  const [menuDisplayed, setMenuDisplayed] = useState(false);
+
   return (
-    <BurgerMenu>
-      <BurgerText>
-        <NavLink to="/">Home page</NavLink>
-      </BurgerText>
-      <br />
-      <BurgerText>
-        <NavLink to="/about">About Me</NavLink>
-      </BurgerText>
-      <br />
+    <BurgerIconHover
+      onMouseEnter={() => setMenuDisplayed(true)}
+      onMouseLeave={() => setMenuDisplayed(false)}
+    >
+      <FontAwesomeIcon icon={faBars} />
+      {menuDisplayed ? (
+        <BurgerMenu>
+          <BurgerText>
+            <NavLink to="/">Home page</NavLink>
+          </BurgerText>
+          <br />
+          <BurgerText>
+            <NavLink to="/about">About Me</NavLink>
+          </BurgerText>
+          <br />
 
-      <BurgerText>
-        <NavLink to="/portfolio">Portfolio</NavLink>
-      </BurgerText>
-      <br />
+          <BurgerText>
+            <NavLink to="/portfolio">Portfolio</NavLink>
+          </BurgerText>
+          <br />
 
-      <BurgerText>
-        <NavLink to="/codepen">Codepens</NavLink>
-      </BurgerText>
-      <br />
+          <BurgerText>
+            <NavLink to="/codepen">Codepens</NavLink>
+          </BurgerText>
+          <br />
 
-      <BurgerText>
-        <NavLink to="/contact">Contact Me</NavLink>
-      </BurgerText>
-    </BurgerMenu>
+          <BurgerText>
+            <NavLink to="/contact">Contact Me</NavLink>
+          </BurgerText>
+        </BurgerMenu>
+      ) : null}
+    </BurgerIconHover>
   );
 };
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import MobileNav from "../components/MobileNav";
 import NavComponent from "../components/Nav";
 import FooterComponent from "../components/Footer";
 
@@ -9,9 +10,7 @@ import AboutPage from "../components/pages/AboutPage";
 import PortfolioPage from "../components/pages/PortfolioPage";
 import ContactPage from "../components/pages/ContactPage";
 import CodepenPage from "../components/pages/CodepenPage";
-
 import NotFoundPage from "../components/pages/NotFoundPage";
-import MobileNav from "../components/MobileNav";
 import LoginPage from "../components/pages/LoginPage";
 import CreateProjectPage from "../components/pages/CreateProjectPage";
 
