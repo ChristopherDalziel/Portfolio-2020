34e68efba06713f0f1f6786f1191f0082cafda92
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-03 17:58:41 +1000

    updated about page to a class component

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -31,50 +31,49 @@ const ResumeButton = styled.button`
   }
 `;
 
-const AboutPage = () => {
-  return (
-    <AboutContainer>
-      <AboutContent>
-        <h1>
-          Hello. <br /> my names Chris.
-        </h1>
-        <br />
-        <p>
-          Over the past 6 years I’ve had the privilege of working as a store
-          manager across 2 businesses gaining experience in many different
-          collaborative environments and situations. 5 months ago, I stepped
-          down from my position to chase a new passion with the hope of finding
-          a new career path. I believe the key to success is really giving it
-          your all in every opportunity you have through every single task,
-          every single day, through hard work anything can be achieved.
+class AboutPage extends React.Component {
+  downloadResume = () => console.log("test");
+  render() {
+    return (
+      <AboutContainer>
+        <AboutContent>
+          <h1>
+            Hello. <br /> my names Chris.
+          </h1>
           <br />
-          <br />
-          Since stepping down I've been working towards a new career path of
-          becoming a web developer and for the past 5 months I've been a
-          fast-track student at Coder Academy Melbourne where I'm honing my
-          skills and knowelege so in a few months I will be ready to join the
-          workforce as a junior developer.
-          <br />
-          <br />
-          Through my expierences of life, work and study I've found my passion
-          for technology, problem solving, collaboration and teamwork. I'm
-          hoping through this new career path I can continue to persue these
-          passions and work towards a career that can be more impactful.
-          <br />
-          <br />
-          <br />
-          <ResumeButton
-            onclick={(e) => {
-              e.preventDefault();
-              console.log("test");
-            }}
-          >
-            DOWNLOAD MY RESUME
-          </ResumeButton>
-        </p>
-      </AboutContent>
-    </AboutContainer>
-  );
-};
+          <p>
+            Over the past 6 years I’ve had the privilege of working as a store
+            manager across 2 businesses gaining experience in many different
+            collaborative environments and situations. 5 months ago, I stepped
+            down from my position to chase a new passion with the hope of
+            finding a new career path. I believe the key to success is really
+            giving it your all in every opportunity you have through every
+            single task, every single day, through hard work anything can be
+            achieved.
+            <br />
+            <br />
+            Since stepping down I've been working towards a new career path of
+            becoming a web developer and for the past 5 months I've been a
+            fast-track student at Coder Academy Melbourne where I'm honing my
+            skills and knowelege so in a few months I will be ready to join the
+            workforce as a junior developer.
+            <br />
+            <br />
+            Through my expierences of life, work and study I've found my passion
+            for technology, problem solving, collaboration and teamwork. I'm
+            hoping through this new career path I can continue to persue these
+            passions and work towards a career that can be more impactful.
+            <br />
+            <br />
+            <br />
+            <ResumeButton onclick={this.downloadResume}>
+              DOWNLOAD MY RESUME
+            </ResumeButton>
+          </p>
+        </AboutContent>
+      </AboutContainer>
+    );
+  }
+}
 
 export default AboutPage;
