6316f5d2376d007a939cf9b86c99cedef509ecd0
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-04 23:31:39 +1000

    added basic styling to login page

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,38 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const LoginContainer = styled.main`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginButton = styled.a`
+  font-family: "Open Sans", sans-serif;
+  background-color: #ef636c;
+  border: none;
+  color: white;
+  padding: 12px 15px;
+  cursor: pointer;
+  font-size: 15px;
+  &:hover {
+    background: #a8b7ab;
+    -webkit-transition: background 0.3s ease-in-out;
+    -moz-transition: background 0.3s ease-in-out;
+    transition: background-color 0.3s ease-in-out;
+  }
+`;
 
 const LoginPage = () => {
   return (
-    <div>
-      <button>Admin Login</button>
-    </div>
+    <LoginContainer>
+      <LoginButton>Admin Login</LoginButton>
+      <p>
+        : If you've found this page and you're not an admin please return to the
+        rest of my portfolio by using the navigation above.
+      </p>
+    </LoginContainer>
   );
 };
 
