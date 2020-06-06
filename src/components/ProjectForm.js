ffea6da9d62d81fa1ec9f0b1b8808021017c0681
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-06 13:10:23 +1000

    Added basic form that takes input, needs to be connected to redux and have a way to pass to the DB, image input to be implimented later

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -1,13 +1,39 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const ProjectFormContainer = styled.div`
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ProjectHeading = styled.h1`
+  float: left;
+  margin: 50px;
+`;
 
 const ProjectForm = () => {
   return (
-    <>
+    <ProjectFormContainer>
+      <ProjectHeading>Create a new project.</ProjectHeading>
       <form>
-        <label>Project Name:</label>
-        <input></input>
+        <label for="projectName">Project Name:</label>
+        <input id="projectName" name="projectName" type="text"></input>
+        <br />
+        <label for="projectDescription">Project Description:</label>
+        <textarea
+          id="projectDescription"
+          name="projectDescription"
+          type="text"
+        ></textarea>
+        <br />
+        <label for="projectDescription">Technology Used:</label>
+        <input id="technologyUsed" name="technologyUsed" type="text"></input>
+        <p>image upload goes here</p>
+        <button type="submit">Submit Project</button>
       </form>
-    </>
+    </ProjectFormContainer>
   );
 };
 
