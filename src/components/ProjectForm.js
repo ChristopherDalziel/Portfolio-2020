d4dbfef90f2238eeb18283e5d829f29c7e10bef5
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-09 13:04:27 +1000

    added files for actions, reducers and updated the form to be a class based component and started working on returning the input values

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -8,27 +8,63 @@ const ProjectFormContainer = styled.div`
   align-items: center;
 `;
 
-const ProjectForm = () => {
-  return (
-    <ProjectFormContainer>
-      <form>
-        <label for="projectName">Project Name:</label>
-        <input id="projectName" name="projectName" type="text"></input>
-        <br />
-        <label for="projectDescription">Project Description:</label>
-        <textarea
-          id="projectDescription"
-          name="projectDescription"
-          type="text"
-        ></textarea>
-        <br />
-        <label for="projectDescription">Technology Used:</label>
-        <input id="technologyUsed" name="technologyUsed" type="text"></input>
-        <p>image upload goes here</p>
-        <button type="submit">Submit Project</button>
-      </form>
-    </ProjectFormContainer>
-  );
-};
+class ProjectForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.project ? props.project.name : "",
+    };
+  }
+
+  onNameChange = (e) => {
+    const name = e.target.value;
+    this.setState(() => {
+      return { name };
+    });
+  };
+
+  onDescriptionChange = () => {};
+
+  onTechnologyChange = () => {};
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    this.props.onSubmit({
+      name: this.state.name,
+    });
+  };
+
+  render() {
+    return (
+      <ProjectFormContainer>
+        <form onSubmit={this.onSubmit}>
+          <label for="projectName">Project Name:</label>
+          <input
+            autoFocus
+            id="projectName"
+            name="projectName"
+            type="text"
+            aria-label="projectName"
+            value={this.state.name}
+            onChange={this.onNameChange}
+          ></input>
+          <br />
+          <label for="projectDescription">Project Description:</label>
+          <textarea
+            id="projectDescription"
+            name="projectDescription"
+            type="text"
+          ></textarea>
+          <br />
+          <label for="projectDescription">Technology Used:</label>
+          <input id="technologyUsed" name="technologyUsed" type="text"></input>
+          <p>image upload goes here</p>
+          <button type="submit">Submit Project</button>
+        </form>
+      </ProjectFormContainer>
+    );
+  }
+}
 
 export default ProjectForm;
