9e0e9dca0b8cf080610e3e8024d1530194dc5c9c
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-16 12:35:06 +1000

    Completed reducer tests

--- src/__tests__/reducers/projects.js
+++ src/__tests__/reducers/projects.js
@@ -5,3 +5,30 @@ test("Should set default state", () => {
   const state = projectReducer(undefined, { type: "@@INIT" });
   expect(state).toEqual([]);
 });
+
+test("Should create a new project", () => {
+  const project = {
+    name: "A new project",
+    description: "Obviously the latest project",
+    technology: "Ruby on Rails",
+    githubUrl: "http//fakegithuburl.com",
+    url: "Image Url",
+  };
+
+  const action = {
+    type: "CREATE_PROJECT",
+    project,
+  };
+
+  const state = projectReducer(projects, action);
+  expect(state).toEqual([...projects, project]);
+});
+
+test("Should set projects", () => {
+  const action = {
+    type: "SET_PROJECTS",
+    projects: [projects[0]],
+  };
+  const state = projectReducer(projects, action);
+  expect(state).toEqual([projects[0]]);
+});
