5b0973b06e24bd921a8e6badc7ea9d173361c80a
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-16 13:02:53 +1000

    Wrote landing page snapshot test

---
+++ src/__tests__/components/LandingPage.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { shallow } from "enzyme";
+import toJSON from "enzyme-to-json";
+import LandingPage from "../../components/pages/LandingPage";
+
+test("Should render Landing Page correctly", () => {
+  const wrapper = shallow(<LandingPage />);
+  expect(toJSON(wrapper)).toMatchSnapshot();
+});
