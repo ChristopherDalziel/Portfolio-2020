86182dba8850cceb2199d89e6fa9bed5bf52f2d9
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-17 20:40:20 +1000

    Added snapshot test to individual project item component

---
+++ src/__tests__/components/Project.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+import toJSON from "enzyme-to-json";
+import projects from "../fixtures/projects";
+import Project from "../../components/Project";
+
+test("Should render individual project with data", () => {
+  const wrapper = shallow(
+    <Project
+      name={projects[2].name}
+      description={projects[2].description}
+      technology={projects[2].technology}
+      githubUrl={projects[2].githubUrl}
+      url={projects[2].url}
+    />
+  );
+  expect(toJSON(wrapper)).toMatchSnapshot();
+});
